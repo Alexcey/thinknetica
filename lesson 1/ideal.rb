54465b41b30819a79db8fba87b95da0fd3daa232
Alexcey <alexcey007@yandex.ru>
2020-01-21 02:18:24 +0300

    task2

--- lesson 1/ideal.rb
+++ lesson 1/ideal.rb
@@ -4,10 +4,10 @@ name = gets.chomp
 p "Напиши свой вес"
 weight = gets.chomp.to_i
 
-ideal = ((weight - 110)*1.15).to_i
+ideal = (weight - 110)*1.15
 
 if ideal > 0 
-  p "Твой идельный вес:  #{ideal}"
+  p "Твой идельный вес: #{ideal}"
 else 
   p "Ваш вес уже оптимальный"
 end
